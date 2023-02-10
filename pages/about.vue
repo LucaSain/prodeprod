65d88ff6a98e591602edf6d64afb3d5044d3bb65
LucaSain <lucasainenco2@gmail.com>
2023-02-10 23:56:38 +0200

    quick fix

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" class="max-w-sm rounded-lg shadow-2xl" />
+            <img src="/nefil/1" class="max-w-sm rounded-lg shadow-2xl" />
             <div>
                 <h1 class="text-5xl font-bold">We are from Moldova!</h1>
                
